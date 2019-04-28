647424bc7ccc10a95eb8fbf54bb2a5d4fc281eae
Ijtaba Hussain <ijtabahussain@live.com>
2019-04-28 15:35:05 +0100

    Initial Sources

---
+++ bit_penalty/common/test.py
@@ -0,0 +1,5 @@
+import utils
+
+test = utils.generate_list(3)
+
+utils.debug_in_binary(test)
---
+++ bit_penalty/common/utils.py
@@ -0,0 +1,45 @@
+import math
+
+base        = 2
+bits_needed = 4
+
+def convert_to_binary( number ):
+  return "{0:b}".format( number ).zfill( bits_needed )
+
+def generate_list( size ):
+  bits_needed = math.ceil( math.log( size, base) )
+  return list( range( size ) )
+
+def debug_in_binary(list):
+
+  previous = None
+
+  print "Item\tBin\tDiff\tPenalty"
+
+  for current in list:
+    binary        = convert_to_binary( current )
+    penalty       = calculate_individual_penalty(previous, current, False)
+    debug_penalty = calculate_individual_penalty(previous, current, True)
+
+    print str(current) + "\t" + str(binary)  + "\t" + str(debug_penalty) + "\t" + str(penalty)
+    previous = current
+
+def calculate_individual_penalty( first, second, verbose ):
+  differing_bits = 0
+  if first is not None:
+        differing_bits = first ^ second
+  if verbose:
+    final = convert_to_binary( differing_bits )
+  else:
+    final = hamming_weight( differing_bits )
+  
+  return final
+
+def hamming_weight( differing_bits ):
+  count = 0
+  
+  while( differing_bits ):
+      differing_bits &= ( differing_bits - 1)
+      count += 1
+
+  return count
